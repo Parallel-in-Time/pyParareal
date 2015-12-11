5c9e026867141e792c850b740ec5d372eab191b0
Daniel Ruprecht <d.ruprecht@leeds.ac.uk>
2015-12-11 14:38:45 +0000

    added basic solution class and some tests

---
+++ src/solution.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy as sp
+
+# Class representing the solution of an IVP
+# M*y' = f(y)
+# with y being an array.
+
+class solution:
+
+  def __init__(self, y, M=0):
+    assert y.ndim==1, "Input y must be a one dimensional array"
+    self.y    = y
+    self.ndof = np.size(y)
+    if isinstance(M,int):
+      self.M = sp.eye(self.ndof)
+    else:
+      self.M = M
+      assert np.array_equal( np.shape(M), [self.ndof, self.ndof]), ".."
+
+  # Overwrite y with a*x+y
+  def axpy(self, a, x):
+    assert a.ndim==1 and np.size(a)==1, "Input a must be a scalar"
+    assert x.ndim==1 and np.size(x)==self.ndof, "Input x must be a vector of same length as y"
+    self.y = a*x + self.y
+
+  # Overwrite y with f(y)
+  def f(self):
+    return 0.0
+
+  # Overwrite y with solution of M*y-alpha*f(y) = b
+  def solve(self, alpha, b):
+    return 0.0
+
+  # Return inf norm of y
+  def norm(self):
+    return np.linalg.norm(self.y, np.inf)
---
+++ tests/test_solution.py
@@ -0,0 +1,78 @@
+import sys
+sys.path.append('../src')
+
+from solution import solution
+from nose.tools import *
+import numpy as np
+import scipy as sp
+
+class TestSolution:
+
+  # Make sure solution can be instantiated and that default M is identity
+  def test_caninstantiate(self):
+    ndof = np.random.randint(255)
+    y = np.random.rand(ndof)
+    sol = solution(y)
+    assert np.array_equal(sol.M, sp.eye(ndof))
+
+  # Make sure exception is raised if y is not 1D array
+  @raises(Exception)
+  def test_failsmatrixy(self):
+    sol = solution(1.0, np.matrix([[1, 1], [1, 1]]))
+
+  # Make sure if matrix M is given as argument to constructor, it is used
+  def test_fistused(self):
+    ndof = np.random.randint(255)
+    M = np.random.rand(ndof, ndof)
+    y = np.random.rand(ndof)
+    sol = solution(y, M)
+    assert np.array_equal(sol.M, M)
+
+  # Make sure exception is raised if size of y and M does not match
+  @raises(Exception)
+  def test_mismatchym(self):
+    ndof = np.random.randint(255)
+    M = np.random.rand(ndof, ndof)
+    y = np.random.rand(ndof-1)
+    sol = solution(y, M)
+
+  # Make sure exception is raised if M has more than two dimensions
+  @raises(Exception)
+  def test_mtoomanydim(self):
+    ndof = np.random.randint(255)
+    M = np.random.rand(ndof, ndof, ndof)
+    y = np.random.rand(ndof)
+    sol = solution(y, M)
+
+  # Make sure axpy performs expected operation
+  def test_mtoomanydim(self):
+    # perform ten tests with random size and values
+    for i in range(0,10):
+      ndof = np.random.randint(255)
+      y = np.random.rand(ndof)
+      x = np.random.rand(ndof)
+      a = np.random.rand(1)
+      sol = solution(y)
+      axpy = a*x+y
+      sol.axpy(a, x)
+      assert np.array_equal(sol.y, axpy)
+
+  # Make sure axpy throws exception if size of does not match y
+  @raises(Exception)
+  def test_yxmismatch(self):
+      ndof = np.random.randint(255)
+      y = np.random.rand(ndof)
+      x = np.random.rand(ndof-2)
+      a = np.random.rand(1)
+      sol = solution(y)
+      sol.axpy(a, x)
+
+  # Make sure axpy throws exception if a is not a scalar
+  @raises(Exception)
+  def test_alphanotscalar(self):
+      ndof = np.random.randint(255)
+      y = np.random.rand(ndof)
+      x = np.random.rand(ndof)
+      a = np.random.rand(3)
+      sol = solution(y)
+      sol.axpy(a, x)  
