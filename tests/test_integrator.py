a2a751fdb9d503bd494e85114949750572c9ef50
Daniel Ruprecht <d.ruprecht@leeds.ac.uk>
2015-12-14 10:30:16 +0000

    added setUp() to integrator test

--- tests/test_integrator.py
+++ tests/test_integrator.py
@@ -8,10 +8,13 @@ import numpy as np
 
 class TestIntegrator(unittest.TestCase):
   
+  def setUp(self):
+    self.t = np.sort( np.random.rand(2) )
+    self.nsteps = np.random.randint(25)
+
   # Make sure integrator can be instantiated
   def test_caninstantiate(self):
-    t = np.sort( np.random.rand(2) )
-    integ = integrator(t[0], t[1], 25)
+    integ = integrator(self.t[0], self.t[1], self.nsteps)
 
   # Make sure instantiation fails if tend is smaller than tstart
   def test_wrongboundsfail(self):
