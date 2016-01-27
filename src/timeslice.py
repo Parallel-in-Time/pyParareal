ad966258f64f4e49f98734ad6c07943301dbfeda
Daniel Ruprecht <d.ruprecht@leeds.ac.uk>
2016-01-27 10:47:40 +0000

    new classes, more tests

---
+++ src/timeslice.py
@@ -0,0 +1,54 @@
+from integrator import integrator
+from solution import solution
+import numpy as np
+
+class timeslice:
+
+  def __init__(self, int_fine, int_coarse, tolerance, iter_max):
+    assert (isinstance(tolerance, float) and tolerance>=0), "Parameter tolerance must be positive or zero"
+    assert (isinstance(iter_max, int) and iter_max>=0), "Parameter iter_max must be a positive integer or zero"
+    assert isinstance(int_fine, integrator), "Parameter int_fine has to be an object of type integrator"
+    assert isinstance(int_coarse, integrator), "Parameter int_coarse has to be an object of type integrator"    
+    assert np.isclose( int_fine.tstart, int_coarse.tstart, rtol = 1e-10, atol=1e-12 ), "Values tstart in coarse and fine integrator must be identical"
+    assert np.isclose( int_fine.tend, int_coarse.tend, rtol = 1e-10, atol=1e-12 ), "Values tend in coarse and fine integrator must be identical"
+    self.int_fine   = int_fine
+    self.int_coarse = int_coarse
+    self.tolerance  = tolerance
+    self.iter_max   = iter_max
+    self.iteration  = 0
+    # Initialize residual such that first check for res < tol always fails
+    self.residual   = tolerance + 1.0
+
+  def update_fine(self):
+    self.sol_fine = self.int_fine.run(self.sol_start)
+
+  def update_coarse(self):
+    self.sol_coarse = self.int_coarse.run(self.sol_start)
+
+  #
+  # GET, SET and IS functions
+  #
+
+  def set_sol_start(self, sol):
+    assert isinstance(sol, solution), "Parameter sol has to be of type solution"
+    self.sol_start = sol
+
+  def get_tstart(self):
+    return self.int_fine.tstart
+
+  def get_tend(self):
+    return self.int_fine.tend
+
+  def get_sol_fine(self):
+    assert hasattr(self, 'fine'), "Timeslice object does not have attribute sol_fine - may be function update_fine was never executed"
+    return self.sol_fine
+
+  def get_sol_coarse(self):
+    assert hasattr(self, 'coarse'), "Timeslice object does not have attribute sol_coarse - may be function update_coarse was never executed"
+    return self.sol_coarse
+
+  def is_converged(self):
+    if ( (self.residual<self.tolerance) or (self.iteration>=self.iter_max) ):
+      return True
+    else:
+      return False
---
+++ tests/test_timeslice.py
@@ -0,0 +1,94 @@
+import sys
+sys.path.append('../src')
+
+from timeslice import timeslice
+from integrator import integrator
+from solution import solution
+import unittest
+import numpy as np
+
+class TestTimeslice(unittest.TestCase):
+
+  def setUp(self):
+    t = np.sort( np.random.rand(2) )
+    nsteps_c        = 1+np.random.randint(25)
+    nsteps_f        = 1+np.random.randint(125)
+    self.int_coarse = integrator(t[0], t[1], nsteps_c)
+    self.int_fine   = integrator(t[0], t[1], nsteps_f)
+    self.ts_default = timeslice(self.int_fine, self.int_coarse, 1e-10, 3)
+
+  # Timeslice can be instantiated
+  def test_caninstantiate(self):
+    ts = timeslice(self.int_fine, self.int_coarse, 1e-10, 5)
+
+  # Negative tolerance throws exception
+  def test_failsnegativetol(self):
+    with self.assertRaises(AssertionError):
+      ts = timeslice(self.int_fine, self.int_coarse, -1e-5, 5)
+
+  # Non-float tolerance throws exception
+  def test_failsintegertol(self):
+    with self.assertRaises(AssertionError):
+      ts = timeslice(self.int_fine, self.int_coarse, 1, 5)
+
+  # Non-int iter_max raises exception
+  def test_failsfloatitermax(self):
+    with self.assertRaises(AssertionError):
+      ts = timeslice(self.int_fine, self.int_coarse, 1e-10, 2.5)
+
+  # Negative iter_max raises exception
+  def test_failsnegativeitermax(self):
+    with self.assertRaises(AssertionError):
+      ts = timeslice(self.int_fine, self.int_coarse, 1e-10, -5)
+
+  # Different values for tstart in fine and coarse integrator raise exception
+  def test_failsdifferenttstart(self):
+    int_c = integrator(1e-10+self.int_coarse.tstart, self.int_coarse.tend, self.int_coarse.nsteps)
+    with self.assertRaises(AssertionError):
+      ts = timeslice(self.int_fine, int_c, 1e-10, 5)
+
+  # Different values for tend in fine and coarse integrator raise exception
+  def test_failsdifferenttend(self):
+    int_c = integrator(self.int_coarse.tstart, 1e-10+self.int_coarse.tend, self.int_coarse.nsteps)
+    with self.assertRaises(AssertionError):
+      ts = timeslice(self.int_fine, int_c, 1e-10, 5)   
+
+  # Directly after initialisation, is_converged returns false unless max_iter = 0
+  def test_initiallynotconverged(self):
+    ts = timeslice(self.int_fine, self.int_coarse, 1e-14+np.random.rand(), 1+np.random.randint(1))
+    assert (not ts.is_converged()), "Directly after initialisation, a is_converged should return False"
+
+  # Directly after initialisation, is_converged returns True if max_iter=0
+  def test_initiallyconvergedifmaxiterzero(self):
+    ts = timeslice(self.int_fine, self.int_coarse, 1e-14+np.random.rand(), 0)
+    assert ts.is_converged(), "If max_iter=0, is_converged should return True"
+  
+  # get_tstart returns correct value
+  def test_get_tstart(self):
+    assert abs(self.ts_default.get_tstart() - self.int_fine.tstart)==0, "get_start returned wrong value"
+
+  # get_tend returns correct value
+  def test_get_tend(self):
+    assert abs(self.ts_default.get_tend() - self.int_fine.tend)==0, "get_start returned wrong value"
+
+  # get_sol_fine without running update_fine before throws exception
+  def test_getfineexception(self):
+    with self.assertRaises(AssertionError):
+      sol_fine = self.ts_default.get_sol_fine()
+
+  # get_sol_coarse without running update_coarse before throws exception
+  def test_getfineexception(self):
+    with self.assertRaises(AssertionError):
+      sol_coarse = self.ts_default.get_sol_coarse()
+
+  # set_sol_start with non-solution objects throws exception
+  def test_solfinenosolutionthrows(self):
+    with self.assertRaises(AssertionError):
+      self.ts_default.set_sol_start(-1)
+
+  # get_sol_fine returns correct value
+  @unittest.skip("Needs implemented integrator")
+  def test_solfine(self):
+    ts = timeslice(self.int_fine, self.int_coarse, 1e-10, 3)
+    sol = solution(np.random.rand(25))
+    ts.set_sol_start(sol)
