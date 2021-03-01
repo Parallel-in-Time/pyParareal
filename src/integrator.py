32d0d8b223c702d85193b83d5681d8d2c8df13b1
Daniel Ruprecht <daniel.ruprecht@tuhh.de>
2021-03-01 16:06:04 +0100

    integrator now allows argument nstep to be either of int or np.integer type

--- src/integrator.py
+++ src/integrator.py
@@ -1,10 +1,11 @@
 from solution import solution
+import numpy as np
 
 class integrator(object):
 
   def __init__(self, tstart, tend, nsteps):
     assert tstart<tend, "tstart must be smaller than tend"
-    assert (isinstance(nsteps, int) and nsteps>0), "nsteps must be a positive integer"
+    assert (isinstance(nsteps, (np.integer, int)) and nsteps>0), "nsteps must be a positive integer"
     self.tstart = tstart
     self.tend   = tend
     self.nsteps = nsteps
