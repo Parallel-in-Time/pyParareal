676e153da8488d68307dc5e0297f759cdd0d6f22
Daniel Ruprecht <daniel.ruprecht@tuhh.de>
2021-03-29 06:56:30 +0200

    a few more outputs in coarsening script

--- scripts/spatial_coarsening/plot_e_norm.py
+++ scripts/spatial_coarsening/plot_e_norm.py
@@ -3,6 +3,7 @@ sys.path.append('../../src')
 import numpy as np
 from numpy import linalg as LA
 import scipy.sparse as sp
+import scipy.linalg as spla
 
 from parareal import parareal
 from impeuler import impeuler
@@ -14,11 +15,20 @@ from pylab import rcParams
 import matplotlib.pyplot as plt
 
 ndof_f = 16
-ndof_c = 12
+ndof_c = 16
+
+xaxis_f = np.linspace(0.0, 1.0, ndof_f+1)[0:ndof_f]
+xaxis_c = np.linspace(0.0, 1.0, ndof_c+1)[0:ndof_c]
+dx_f = xaxis_f[1] - xaxis_f[0]
+dx_c = xaxis_c[1] - xaxis_c[0]
+
 u0_f   = np.ones(ndof_f)
 u0_c   = np.ones(ndof_c)
-A_f    = (1.0/(ndof_f-1))*sp.diags([-np.ones(ndof_f), np.ones(ndof_f)], [-1,0], shape=(ndof_f, ndof_f))
-A_c    = np.eye(ndof_c)
+col    = np.zeros(ndof_f)
+col[0] = 1.0
+col[1] = -1.0
+A_f    = (1.0/dx_f)*spla.circulant(col)
+A_c    = (1.0/dx_c)*spla.circulant(col[0:ndof_c])
 
 u0fine   = solution_linear(u0_f, A_f)
 u0coarse = solution_linear(u0_c, A_c)
@@ -27,51 +37,46 @@ Tend    = 1.0
 nslices = 8
 tol     = 0.0
 maxiter = 4
-nfine   = 32
-ncoarse = 2
+nfine   = 1
+ncoarse = 1
+
+
 
 para = parareal(0.0, Tend, nslices, trapezoidal, impeuler, nfine, ncoarse, tol, maxiter, u0fine, u0coarse)
 E, Mginv = para.get_parareal_matrix()
 F = para.timemesh.slices[0].get_fine_update_matrix(u0fine)
 G = para.timemesh.slices[0].get_coarse_update_matrix(u0coarse)
-
-print(np.linalg.norm(E.todense(), 2))
-
-## DEFINE G BY MATRIX: compute low rank approximation for F and use this as coarse propagator ... what is the resulting E ? 
-u, s, vh = LA.svd(E.todense())
-print(np.max(np.abs(s)))
-
-
-# confirm that the fine propagator matrix has full rank
 rank_F = LA.matrix_rank(F.todense())
-print("rank F: %5.3i" % rank_F)
+if not rank_F==ndof_f:
+  print("Rank of F is not equal to ndof_f")
+rank_C = LA.matrix_rank(G)
+if not rank_C==ndof_c:
+  print("Rank of G is not equal to ndof_c")
+rank_E = LA.matrix_rank(E.todense())
+if not rank_E==nslices*ndof_f:
+  print("Rank of E is not equal to nslices x ndof_f: rank(E) = %3i" % rank_E)
+  
+Enorm_2   = np.linalg.norm(E.todense(), 2)
+Enorm_1   = np.linalg.norm(E.todense(), 1)
+Enorm_inf = np.linalg.norm(E.todense(), np.inf)
+print("Inf-Norm(E): %5.3e" % Enorm_inf)
 
-# number of coarse DoF has no bearing on rank of E, it seems
-# rank should be ndof_f*nslices
-print("rank E: %5.3i" % LA.matrix_rank(E.todense()))
-print(np.shape(E.todense()))
-print("ndof_f x nslices: %5.3i" % (ndof_f*nslices))
-
-print("==========")
-print("1-norm of E: %5.3e" % np.linalg.norm(E.todense(), 1))
-print("inf-norm of E: %5.3e" % np.linalg.norm(E.todense(), np.inf))
-
-print("==========")
+# Compute SVD of F
 u_f, s_f, vh_f = LA.svd(F.todense())
-w, v = LA.eig(F.todense())
-print("Inf-Norm of E: %5.3e" % np.linalg.norm(E.todense(), np.inf))
-print("Inf-Norm of F-G: %5.3e" % np.linalg.norm(F-G, np.inf))
-print("m+1 SV of F: %5.3e" % s_f[ndof_c])
-print("min SV of F: %5.3e" % s_f[-1])
+w, v           = LA.eig(F.todense())
+#print("2-Norm of E:   %5.3e" % np.linalg.norm(E.todense(), 2))
+#print("2-Norm of F-G: %5.3e" % np.linalg.norm(F-G, 2))
+#print("m+1 SV of F:   %5.3e" % s_f[ndof_c])
+#print("min SV of F:   %5.3e" % s_f[-1])
 
 fig = plt.figure()
-plt.plot(s_f, 'bo', label='SV')
-plt.plot(w, 'rx', label='EV')
-plt.ylim([0.9, 1.1])
+plt.semilogy(np.sort(s_f), 'bo', label='SV')
+plt.plot(np.sort(np.abs(w)), 'rx', label='EV')
+plt.plot(np.zeros(ndof_f) + Enorm_2, 'k--', label='Norm(E,2)')
+#plt.ylim([0.9, 1.1])
+plt.legend()
 plt.show()
 
-# NOTE: numerically it seems that the SV of F are all >= 1
-
-print("1-norm of F-G: %5.3e" % np.linalg.norm(F-G, 1))
-print("2-norm of F-G: %5.3e" % np.linalg.norm(F-G, 2))
-print("inf-norm of F-G: %5.3e" % np.linalg.norm(F-G, np.inf))
+## The bound || E ||_2 >= || F - G ||_2 is reasonably sharp for small number of time slices.
+## As the number of time slices increases, the norm of E becomes much larger than || F - G ||_2
+## This is probably related to the B^k = G^k (F - G) matrices.
