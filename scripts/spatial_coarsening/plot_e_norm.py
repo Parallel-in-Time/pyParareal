3a29855e7d6e94a90e97001655f9e424284093cf
Daniel Ruprecht <daniel.ruprecht@tuhh.de>
2021-07-20 15:45:19 +0200

    more scripts for spatial coarsening

--- scripts/spatial_coarsening/plot_e_norm.py
+++ scripts/spatial_coarsening/plot_e_norm.py
@@ -14,8 +14,15 @@ from solution_linear import solution_linear
 from pylab import rcParams
 import matplotlib.pyplot as plt
 
-ndof_f = 16
-ndof_c = 15
+Tend    = 0.4
+nslices = 8
+tol     = 0.0
+maxiter = 7
+nfine   = 5
+ncoarse = 5
+
+ndof_f = 64
+ndof_c = 32
 
 xaxis_f = np.linspace(0.0, 1.0, ndof_f+1)[0:ndof_f]
 xaxis_c = np.linspace(0.0, 1.0, ndof_c+1)[0:ndof_c]
@@ -28,8 +35,8 @@ col    = np.zeros(ndof_f)
 # First order upwind
 col[0] = 1.0
 col[1] = -1.0
-A_f    = (1.0/dx_f)*spla.circulant(col)
-A_c    = (1.0/dx_c)*spla.circulant(col[0:ndof_c])
+A_f    = -(1.0/dx_f)*spla.circulant(col)
+A_c    = -(1.0/dx_c)*spla.circulant(col[0:ndof_c])
 # Second order centered
 #col[1]  = -1.0
 #col[-1] = 1.0
@@ -38,19 +45,11 @@ A_c    = (1.0/dx_c)*spla.circulant(col[0:ndof_c])
 u0fine   = solution_linear(u0_f, A_f)
 u0coarse = solution_linear(u0_c, A_c)
 
-Tend    = 1.0
-nslices = 8
-tol     = 0.0
-maxiter = 4
-nfine   = 1
-ncoarse = 1
-
-
-
-para     = parareal(0.0, Tend, nslices, trapezoidal, impeuler, nfine, ncoarse, tol, maxiter, u0fine, u0coarse)
+para     = parareal(0.0, Tend, nslices, impeuler, impeuler, nfine, ncoarse, tol, maxiter, u0fine, u0coarse)
 E, Mginv = para.get_parareal_matrix()
 F        = para.timemesh.slices[0].get_fine_update_matrix(u0fine)
 G        = para.timemesh.slices[0].get_coarse_update_matrix(u0coarse)
+'''
 rank_F   = LA.matrix_rank(F.todense())
 if not rank_F==ndof_f:
   print("Rank of F is not equal to ndof_f")
@@ -60,27 +59,27 @@ if not rank_C==ndof_c:
 rank_E = LA.matrix_rank(E.todense())
 if not rank_E==nslices*ndof_f:
   print("Rank of E is not equal to nslices x ndof_f: rank(E) = %3i" % rank_E)
-  
+  '''
 Enorm_2   = np.linalg.norm(E.todense(), 2)
 Enorm_1   = np.linalg.norm(E.todense(), 1)
 Enorm_inf = np.linalg.norm(E.todense(), np.inf)
 print("Inf-Norm(E): %5.3e" % Enorm_inf)
 
 # Compute SVD of F
-u_f, s_f, vh_f = LA.svd(F.todense())
-w, v           = LA.eig(F.todense())
+#u_f, s_f, vh_f = LA.svd(F.todense())
+#w, v           = LA.eig(F.todense())
 #print("2-Norm of E:   %5.3e" % np.linalg.norm(E.todense(), 2))
 #print("2-Norm of F-G: %5.3e" % np.linalg.norm(F-G, 2))
 #print("m+1 SV of F:   %5.3e" % s_f[ndof_c])
 #print("min SV of F:   %5.3e" % s_f[-1])
 
-fig = plt.figure()
-plt.semilogy(np.sort(s_f), 'bo', label='SV')
-plt.plot(np.sort(np.abs(w)), 'rx', label='EV')
-plt.plot(np.zeros(ndof_f) + Enorm_2, 'k--', label='Norm(E,2)')
-#plt.ylim([0.9, 1.1])
-plt.legend()
-plt.show()
+#fig = plt.figure()
+#plt.semilogy(np.sort(s_f), 'bo', label='SV')
+#plt.plot(np.sort(np.abs(w)), 'rx', label='EV')
+#plt.plot(np.zeros(ndof_f) + Enorm_2, 'k--', label='Norm(E,2)')
+##plt.ylim([0.9, 1.1])
+#plt.legend()
+#plt.show()
 
 ## The bound || E ||_2 >= || F - G ||_2 is reasonably sharp for small number of time slices.
 ## As the number of time slices increases, the norm of E becomes much larger than || F - G ||_2
---
+++ scripts/spatial_coarsening/plot_norm_vs_ncoarse.py
@@ -0,0 +1,126 @@
+import sys
+sys.path.append('../../src')
+import numpy as np
+from numpy import linalg as LA
+import scipy.sparse as sp
+import scipy.linalg as spla
+import scipy.sparse.linalg as linalg
+
+from parareal import parareal
+from impeuler import impeuler
+from intexact import intexact
+from trapezoidal import trapezoidal
+from solution_linear import solution_linear
+
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from subprocess import call
+
+def uex(x,t):
+  return np.exp(-(x-1.0-t)**2/0.1**2)
+  
+Tend    = 0.4
+nslices = 8
+tol     = 0.0
+maxiter = 7
+nsteps  = [1, 2, 4, 6, 8, 10]
+
+ndof_f   = 64
+ndof_c_v = [32, 48, 63, 64]
+
+xaxis_f = np.linspace(0.0, 2.0, ndof_f+1)[0:ndof_f]
+dx_f = xaxis_f[1] - xaxis_f[0]
+u0_f   = uex(xaxis_f, 0.0)
+col    = np.zeros(ndof_f)
+# 1 = advection with implicit Euler / upwind FD
+# 2 = advection with trapezoidal rule / centered FD
+# 3 = diffusion with trapezoidal rule / centered second order FD
+problem = 3
+
+if problem==1:
+  col[0] = 1.0
+  col[1] = -1.0
+  A_f    = -(1.0/dx_f)*spla.circulant(col)
+  # non-periodic boundary conditions instead
+  #A_f[0,-1] = 0.0
+elif problem==2:
+  col[1]  = -1.0
+  col[-1] = 1.0
+  A_f = -(1.0/(2.0*dx_f))*spla.circulant(col)
+elif problem==3:
+  col[0]  = -2.0
+  col[1]  = 1.0
+  col[-1] = 1.0
+  A_f = (1/dx_f**2)*spla.circulant(col)
+else:
+  quit()
+A_f = sp.csc_matrix(A_f)
+
+D = A_f*A_f.H - A_f.H*A_f
+print("Normality number: %5.3f" % np.linalg.norm(D.todense()))
+u0fine   = solution_linear(u0_f, A_f)
+norm_l2  = np.zeros((4,np.size(nsteps)))
+norm_inf = np.zeros((4,np.size(nsteps)))
+for nn in range(4):
+
+  ndof_c  = ndof_c_v[nn]
+  xaxis_c = np.linspace(0.0, 2.0, ndof_c+1)[0:ndof_c]
+  dx_c    = xaxis_c[1] - xaxis_c[0]
+  u0_c    = uex(xaxis_c, 0.0)
+  col     = np.zeros(ndof_c)
+  
+  if problem==1:
+    # First order upwind
+    col[0] = 1.0
+    col[1] = -1.0
+    A_c    = -(1.0/dx_c)*spla.circulant(col[0:ndof_c])
+    # non-periodic BC instead
+    #A_c[0,-1] = 0.0
+  # Second order centered
+  elif problem==2:
+
+    col = np.zeros(ndof_c)
+    col[1] = -1.0
+    col[-1] = 1.0
+    A_c = -(1.0/(2.0*dx_c))*spla.circulant(col)
+  elif problem==3:
+    col = np.zeros(ndof_c)
+    col[0] = -2.0
+    col[1] = 1.0
+    col[-1] = 1.0
+    A_c = (1/dx_c**2)*spla.circulant(col)
+  u0coarse = solution_linear(u0_c, A_c)
+  
+  for mm in range(np.size(nsteps)):
+  
+    if problem==1:
+      para     = parareal(0.0, Tend, nslices, impeuler, impeuler, nsteps[mm], nsteps[mm], tol, maxiter, u0fine, u0coarse)
+    else:
+      para     = parareal(0.0, Tend, nslices, trapezoidal, trapezoidal, nsteps[mm], nsteps[mm], tol, maxiter, u0fine, u0coarse)
+    Pmat, Bmat = para.get_parareal_matrix()
+    ### Parareal iteration: y^k+1 = Pmat*y^k + Bmat*b
+    norm_l2[nn,mm] = np.linalg.norm(Pmat.todense(), 2)
+    norm_inf[nn,mm] = np.linalg.norm(Pmat.todense(), np.inf)
+fig = plt.figure(1)
+plt.plot(nsteps, norm_l2[0,:], 'bo-', label='m='+str(ndof_c_v[0]))
+plt.plot(nsteps, norm_l2[1,:], 'rx-', label='m='+str(ndof_c_v[1]))
+plt.plot(nsteps, norm_l2[2,:], 'cd-', label='m='+str(ndof_c_v[2]))
+plt.plot(nsteps, norm_l2[3,:], 'k+-', label='m='+str(ndof_c_v[3]))
+#plt.plot(nsteps, norm_inf[0,:], 'b+--')
+#plt.plot(nsteps, norm_inf[1,:], 'r+--')
+#plt.plot(nsteps, norm_inf[2,:], 'c+--')
+
+plt.legend()
+plt.xlabel('Number of coarse/fine steps per slice')
+plt.ylabel(r'$|| E ||_2$')
+#plt.ylim([1e-15, 1e1])
+#plt.xlabel([0, maxiter])
+if problem==1:
+  filename='parareal-coarsening-advection-upwind-norm.pdf'
+elif problem==2:
+  filename='parareal-coarsening-advection-centered-norm.pdf'
+elif problem==3:
+  filename = 'parareal-coarsening-heat-norm.pdf'
+plt.gcf().savefig(filename, bbox_inches='tight')
+call(["pdfcrop", filename, filename])
+plt.show()
