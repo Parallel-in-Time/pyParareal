006719eac20044be0e79188c2a3db6c200b4e45e
Daniel Ruprecht <daniel.ruprecht@tuhh.de>
2021-08-18 15:50:19 +0200

    updated figure to plot eigenvalues so that generated files need not be rescaled to fix in journal

--- scripts/spatial_coarsening/plot_ev_sv.py
+++ scripts/spatial_coarsening/plot_ev_sv.py
@@ -3,6 +3,7 @@ import scipy.linalg as LA
 from get_matrix import get_upwind, get_centered, get_diffusion
 from matplotlib import pyplot as plt
 from subprocess import call
+from pylab import rcParams
 
 nx = 20
 h  = 1.0
@@ -16,18 +17,21 @@ eigs_exp, dummy = LA.eig(A_exp.todense())
 A_exp_fine = LA.expm(A_upwind*0.1*dt)
 eigs_exp_fine, dummy = LA.eig(A_exp_fine.todense())
 
+rcParams['figure.figsize'] = 2.5, 2.5
+fs = 8
+ms = 4
 fig = plt.figure(1)
 plt.plot(0*eigs.real, np.linspace(-2.1,2.1,nx), 'k', linewidth=2)
-plt.plot(eigs.real, eigs.imag, 'bo', markersize=8, markerfacecolor='b', label='$\lambda(A)$')
-plt.plot(eigs_exp.real, eigs_exp.imag, 'ro', markersize=8, markerfacecolor='r', label='$\exp(\lambda(A))$')
-plt.plot(eigs_exp_fine.real, eigs_exp_fine.imag, 'rx', markersize=4, markerfacecolor='r', label='$\exp(\lambda(A)/10)$')
+plt.plot(eigs.real, eigs.imag, 'bo', markersize=ms, markerfacecolor='b', label='$\lambda(A)$')
+plt.plot(eigs_exp.real, eigs_exp.imag, 'ro', markersize=ms, markerfacecolor='r', label='$\exp(\lambda(A))$')
+plt.plot(eigs_exp_fine.real, eigs_exp_fine.imag, 'rx', markersize=ms, markerfacecolor='r', label='$\exp(\lambda(A)/10)$')
 circle = plt.Circle((0.0,0.0), 1.0, color='k', fill=False)
 fig.gca().add_patch(circle)
 plt.xlim([-2.1, 2.1])
 plt.ylim([-2.1, 2.1])
-plt.xlabel('Re($\lambda$)')
-plt.ylabel('Im($\lambda$)')
-plt.legend()
+plt.xlabel('Re($\lambda$)', fontsize=fs)
+plt.ylabel('Im($\lambda$)', fontsize=fs)
+plt.legend(loc='lower right', fontsize=fs, prop={'size':fs-2}, handlelength=3)
 filename='upwind-eig.pdf'
 plt.gcf().savefig(filename, bbox_inches='tight')
 call(["pdfcrop", filename, filename])
@@ -61,16 +65,16 @@ print("Normality number: %5.3e" % np.linalg.norm(D.todense()))
 
 fig = plt.figure(2)
 plt.plot(0*eigs.real, np.linspace(-2.1,2.1,nx), 'k', linewidth=2)
-plt.plot(eigs.real, eigs.imag, 'bo', markersize=8, markerfacecolor='b', label='$\lambda(A)$')
-plt.plot(eigs_exp.real, eigs_exp.imag, 'ro', markersize=8, markerfacecolor='r', label='$\exp(\lambda(A))$')
-plt.plot(eigs_exp_fine.real, eigs_exp_fine.imag, 'rx', markersize=4, markerfacecolor='r', label='$\exp(\lambda(A)/10)$')
+plt.plot(eigs.real, eigs.imag, 'bo', markersize=ms, markerfacecolor='b', label='$\lambda(A)$')
+plt.plot(eigs_exp.real, eigs_exp.imag, 'ro', markersize=ms, markerfacecolor='r', label='$\exp(\lambda(A))$')
+plt.plot(eigs_exp_fine.real, eigs_exp_fine.imag, 'rx', markersize=ms, markerfacecolor='r', label='$\exp(\lambda(A)/10)$')
 circle = plt.Circle((0.0,0.0), 1.0, color='k', fill=False)
 fig.gca().add_patch(circle)
 plt.xlim([-2.1, 2.1])
 plt.ylim([-2.1, 2.1])
-plt.xlabel('Re($\lambda$)')
-plt.ylabel('Im($\lambda$)')
-plt.legend()
+plt.xlabel('Re($\lambda$)', fontsize=fs)
+plt.ylabel('Im($\lambda$)', fontsize=fs)
+plt.legend(loc='lower right', fontsize=fs, prop={'size':fs-2}, handlelength=3)
 filename='centered-eig.pdf'
 plt.gcf().savefig(filename, bbox_inches='tight')
 call(["pdfcrop", filename, filename])
