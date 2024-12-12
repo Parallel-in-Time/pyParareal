c27edc7effd026b458f907932f68bfbd92717726
Daniel Ruprecht <ruprecht@tuhh.de>
2024-12-12 08:52:24 +0100

    test solution riemann now works with pytest

--- tests/test_solution_riemann.py
+++ tests/test_solution_riemann.py
@@ -1,34 +1,40 @@
 import sys
-sys.path.append('../src')
+sys.path.append('./src')
+sys.path.append('./tests')
 
 from solution import solution
 from solution_riemann import solution_riemann
-from test_solution import TestSolution
-from nose.tools import *
-import unittest
+import pytest
 import numpy as np
 from scipy import sparse
 
-class test_solution_riemann(TestSolution):
+class TestClass:
 
   def setUp(self):
-    super(test_solution_riemann, self).setUp()
+    self.ndof = np.random.randint(255)
+    self.M = np.random.rand(self.ndof, self.ndof)
+    self.y = np.random.rand(self.ndof)
+    self.x = np.random.rand(self.ndof)
     y = np.ones(15)
 
   # Can instantiate
   def test_caninstantiate(self):
+    self.setUp()               
     sol_rm = solution_riemann(self.y, dx=0.1)
 
   # Solve works correctly
   def test_solve_alpha_zero(self):
+    self.setUp()                
     sol_rm = solution_riemann(self.y, dx=0.1)
     sol_rm.solve(0.0)
     
-  @raises(Exception)
   def test_solve_alpha_notzero(self):
+    self.setUp()                
     sol_rm = solution_riemann(self.y, dx=0.1)
-    sol_rm.solve(1.0)
+    with pytest.raises(Exception):
+      sol_rm.solve(1.0)
 
   def test_can_evaluate_f(self):
+    self.setUp()                
     sol_rm = solution_riemann(self.y, dx=0.1)
     sol_rm.f()
