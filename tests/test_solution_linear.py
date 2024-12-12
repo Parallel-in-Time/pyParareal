e51f0f44033a65322d1e83091ef7f6fdb0ee5644
Daniel Ruprecht <ruprecht@tuhh.de>
2024-12-12 08:53:46 +0100

    test solution linear now works with pytest

--- tests/test_solution_linear.py
+++ tests/test_solution_linear.py
@@ -1,32 +1,36 @@
 import sys
-sys.path.append('../src')
+sys.path.append('./src')
 
 from solution import solution
 from solution_linear import solution_linear
-from test_solution import TestSolution
-from nose.tools import *
-import unittest
+import pytest
 import numpy as np
 from scipy import sparse
 
-class test_solution_linear(TestSolution):
+class TestClass:
 
   def setUp(self):
-    super(test_solution_linear, self).setUp()
+    self.ndof = np.random.randint(255)
+    self.M = np.random.rand(self.ndof, self.ndof)
+    self.y = np.random.rand(self.ndof)
+    self.x = np.random.rand(self.ndof)
     self.A = sparse.spdiags([ np.ones(self.ndof), -2.0*np.ones(self.ndof), np.ones(self.ndof)], [-1,0,1], self.ndof, self.ndof)
 
   # Can instantiate
   def test_caninstantiate(self):
+    self.setUp()                   
     sol_lin = solution_linear(self.y, self.A, self.M)
 
   # Throws if size of matrix A does not match y and M
   def test_wrongsizeA(self):
+    self.setUp()                   
     A = np.random.rand(self.ndof+1, self.ndof+1)
-    with self.assertRaises(AssertionError):
+    with pytest.raises(AssertionError):
       sol_lin = solution_linear(self.y, A, self.M)
 
   # Solve works correctly
   def test_solve(self):
+    self.setUp()                   
     self.M = sparse.spdiags([ np.random.rand(self.ndof) ], [0], self.ndof, self.ndof)*sparse.identity(self.ndof)
     self.A = sparse.csc_matrix(self.A)
     u = np.reshape(np.random.rand(self.ndof), (self.ndof,1))
